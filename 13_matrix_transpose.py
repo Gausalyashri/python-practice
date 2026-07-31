14e205f54435fa7c780368d6591fd6bb92aac06f
Gausalyashri <gausiselvarasu@gmail.com>
2026-07-31 22:25:26 +0530

    Add files via upload

---
+++ 13_matrix_transpose.py
@@ -0,0 +1,11 @@
+"""Problem: Matrix Transpose
+Given a 2D matrix (list of lists), return its transpose.
+"""
+
+def transpose(matrix):
+    return [list(row) for row in zip(*matrix)]
+
+
+if __name__ == "__main__":
+    m = [[1, 2, 3], [4, 5, 6]]
+    print(transpose(m))  # [[1, 4], [2, 5], [3, 6]]
