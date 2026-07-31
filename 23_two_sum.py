1312f742a0e01f95b3b8bf541b39095031778869
Gausalyashri <gausiselvarasu@gmail.com>
2026-07-31 22:29:03 +0530

    Add files via upload

---
+++ 23_two_sum.py
@@ -0,0 +1,19 @@
+"""Problem: Two Sum
+Given a list of integers and a target sum, return the indices
+of the two numbers that add up to the target, using a single
+pass hash-map approach for O(n) time.
+"""
+
+def two_sum(nums, target):
+    seen = {}
+    for i, num in enumerate(nums):
+        complement = target - num
+        if complement in seen:
+            return [seen[complement], i]
+        seen[num] = i
+    return None
+
+
+if __name__ == "__main__":
+    print(two_sum([2, 7, 11, 15], 9))   # [0, 1]
+    print(two_sum([3, 2, 4], 6))        # [1, 2]
